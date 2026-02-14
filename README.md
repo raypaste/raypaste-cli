0d91d189665f0a46944873219a92943f04da6a64
Winston Hsiao <hsiaowinston@gmail.com>
2026-02-14 12:57:25 -0500

    Initial CLI setup using Cobra

---
+++ README.md
@@ -0,0 +1,3 @@
+# raypaste-cli
+
+Ultra-fast AI revised meta prompts from your input text in your terminal.
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2026 Raypaste
+*/
+package main
+
+import "raypaste-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
